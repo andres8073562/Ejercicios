645de4dec1042471c2389c99b2c435e2ec4cae4a
andres8073562 <espineldavid56@gmail.com>
2025-02-20 08:12:58 -0500

    Update Entrenamineto_Emocional.m

---
+++ Entrenamineto_Emocional.m
@@ -0,0 +1,17 @@
+## 	Entrenamiento Emocional:
+Entrenar a las personas para detectar costumbres de gestión emocional que no les ayudan en su día a día y aprendan a cambiarlas por otras que sí les ayuden a sentirse mejor**
+## Beneficios:
+-  identificar y cambiar patrones de gestión emocional que no favorecen el bienestar
+-   Aprender a manejar las emociones de manera más saludable
+-   Mejorar la autoestima y la autonomía
+-   Mejorar la memoria y la agilidad mental
+-   Promover sensaciones como el optimismo y la euforia
+-   Mejorar la calidad de vida
+## INstrucciones:
+1. Acepta y reconoce tus emociones sin juzgarte
+2. Analiza las situaciones desde diferentes puntos de vista
+3.  Reemplaza los pensamientos negativos por positivos
+
+## Consejos:
+-  Conoce tus emociones. El primer paso para gestionar las emociones es saber reconocerlas. ...
+-  Autorreflexión. ...
